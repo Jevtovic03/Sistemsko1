10abebd2e2830b8a1569a4f57fd4f9ed2518ca1f
Jevtovic03 <vojin.s.jevtovic@gmail.com>
2024-06-20 03:28:06 +0200

    Add files via upload

---
+++ Zad2Sistemsko/HttpServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using Zad2Sistemsko;
+
+namespace Zad2Sistemsko
+{
+    internal class HttpServer
+    {
+        private readonly HttpListener listener;
+        public HttpServer(string prefix)
+        {
+            listener = new HttpListener();
+            listener.Prefixes.Add(prefix);
+            listener.Start();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Web server pokrenut. Osluškujem zahteve...");
+            while (true)
+            {
+                var context = listener.GetContext();
+                Task.Run(async () => {
+                    Console.WriteLine("Zahtev primljen, obradjivanje...");
+                    await FileProcessor.CountOccurrencesAsync(context);
+                });
+            }
+        }
+    }
+}
